bf6870ca0b45f682ec12ec7cc6983f4380bc3580
Emanuel E. Sosa <emanuelesausosa@gmail.com>
2020-11-26 19:17:10 -0600

    add table songs to album

--- src/app/features/album/album.component.ts
+++ src/app/features/album/album.component.ts
@@ -5,4 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './album.component.html',
   styleUrls: ['./album.component.scss'],
 })
-export class AlbumComponent {}
+export class AlbumComponent {
+  albumModel: any = {
+    year: 1999,
+    title: 'Live At Fire Australia',
+    songs: [
+      {
+        num: 1,
+        title: 'Cancion 1',
+        featuring: 'Bon Jovi',
+      },
+      {
+        num: 2,
+        title: 'Cancion 2',
+        featuring: 'Bon Jovi',
+      },
+      {
+        num: 3,
+        title: 'Cancion 3',
+        featuring: 'Elton Jonh',
+      },
+    ],
+  };
+}
