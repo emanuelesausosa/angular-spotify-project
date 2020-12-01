09171f07562edd460b8efcbb175d60795a17bc59
Emanuel E. Sosa <emanuelesausosa@gmail.com>
2020-11-30 18:55:56 -0600

    add routing module def

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AlbumComponent } from './features/album/album.component';
 import { SongDetailComponent } from './features/song-detail/song-detail.component';
+import { FooComponent } from './features/foo/foo.component';
 
 @NgModule({
-  declarations: [AppComponent, AlbumComponent, SongDetailComponent],
+  declarations: [
+    AppComponent,
+    AlbumComponent,
+    SongDetailComponent,
+    FooComponent,
+  ],
   imports: [BrowserModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AlbumComponent } from './features/album/album.component';
+import { FooComponent } from './features/foo/foo.component';
+import { SongDetailComponent } from './features/song-detail/song-detail.component';
+
+const routes: Routes = [
+  {
+    path: 'album',
+    component: AlbumComponent,
+  },
+  {
+    path: 'foo',
+    component: FooComponent,
+  },
+  {
+    path: 'song/:id',
+    component: SongDetailComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
